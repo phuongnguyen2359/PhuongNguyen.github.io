5ff3822a254a410ba09d912fe84e4ae22ee024c5
Phuong Nguyen <nguyenphuong.it242@gmail.com>
2019-05-30 11:20:48 +0700

    1st commit

---
+++ build/precache-manifest.2d8a35461acccced729626806dd9c4ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945b762036663ad831ea849d4c2acc9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76618ed549ee6f34f41b",
+    "url": "/static/css/main.c6688633.chunk.css"
+  },
+  {
+    "revision": "3189aa0d2ff938013bec",
+    "url": "/static/js/2.881e6bbf.chunk.js"
+  },
+  {
+    "revision": "76618ed549ee6f34f41b",
+    "url": "/static/js/main.904a37c0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "822e2bb2c438031446d36b0487bffe15",
+    "url": "/static/media/loading.822e2bb2.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
